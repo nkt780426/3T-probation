088b516907fceaf1b27a94ede37be4eeded70764
Võ Nguyễn Hoàng <93706591+nkt780426@users.noreply.github.com>
2024-10-31 14:32:01 +0700

    Delete tmp directory

--- tmp/redis/check_last_ts_ms.py
+++
@@ -1,9 +0,0 @@
-from redis import Redis
-import time
-
-# Connect to Redis server
-redis_client = Redis(host='localhost', port=6379, db=0)
-
-while True:
-    print(redis_client.get('probation:last_ts_ms').decode())
-    time.sleep(1)
